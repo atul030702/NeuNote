d37bd3916fc25a39bb0d18a342d7f73da39e8bd9
atul030702 <singhatul0307@gmail.com>
2025-07-21 00:26:02 +0530

    exported form validation function to utils file

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -4,14 +4,13 @@ import { useState, useTransition } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Loader2 } from "lucide-react";
+import { Eye, EyeOff } from "lucide-react";
 
-import { toast } from "sonner";
 import { CardContent, CardFooter } from "./ui/card";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { loginAction, signUpAction } from "@/actions/users";
-import { checkValidUserData } from "@/utils/validateData";
+import handleSubmit from "@/utils/handleFormSubmit";
 
 type Props = {
     type: "login" | "signup",
@@ -19,64 +18,25 @@ type Props = {
 
 const AuthForm: React.FC<Props> = ({ type }) => {
     const [clientValidationError, setClientValidationError] = useState<string | null>(null);
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [showConfirmPs, setShowConfirmPs] = useState<boolean>(false);
     const [isPending, startTransition] = useTransition();
     const router = useRouter();
 
     const isLoginForm = type === "login";
-
-    const handleSubmit = (formData: FormData) => {
-        const email = formData?.get("email") as string;
-        const password = formData?.get("password") as string;
-        const confirmPassword = formData?.get("confirmPassword") as string;
-        const name = formData.get("name") as string;
-
-        // client side form validation on sign-up
-        if(!isLoginForm) {
-            const validationError = checkValidUserData(email, password, name, confirmPassword);
-
-            if(validationError) {
-                setClientValidationError(validationError);
-                return;
-            }
-        }
-        // clear the previous client error for login page
-        setClientValidationError(null);
-
-        startTransition(async () => {
-            let errorMessage;
-            let title;
-            let description;
-
-            if(isLoginForm) {
-                errorMessage = (await loginAction(email, password)).errorMessage;
-                title = "Logged In";
-                description = "You have been successfully logged in";
-            }else {
-                errorMessage = (await signUpAction(email, password, name)).errorMessage;
-                title = "Signed Up";
-                description = "Check your email for confirmation link"; 
-            }
-
-            if(!errorMessage) {
-                toast.success(title, {
-                    description: description,
-                });
-
-                router.replace("/");
-            }else {
-                toast.error("Error", {
-                    description: errorMessage,
-                });
-            }
-        });
-    };
 
     return (
         <form 
             onSubmit={(e) => {
                 e.preventDefault();
                 const formData = new FormData(e.currentTarget);
-                handleSubmit(formData);
+                handleSubmit(
+                    formData, 
+                    isLoginForm, 
+                    router, 
+                    setClientValidationError, 
+                    startTransition
+                );
             }}
         >
             <CardContent className="w-full grid items-center gap-4">
@@ -90,7 +50,7 @@ const AuthForm: React.FC<Props> = ({ type }) => {
                             type="name"
                             required
                             disabled={isPending}
-                        />
+                        />  
                     </div>
                 )}
 
@@ -108,27 +68,49 @@ const AuthForm: React.FC<Props> = ({ type }) => {
 
                 <div className="flex flex-col space-y-1.5">
                     <Label htmlFor="password">Password</Label>
-                    <Input
-                        id="password"
-                        name="password"
-                        placeholder="Enter your password"
-                        type="password"
-                        required
-                        disabled={isPending}
-                    />
+                    <div className="relative">
+                        <Input
+                            id="password"
+                            name="password"
+                            placeholder="Enter your password"
+                            type={showPassword ? "text" : "password"}
+                            required
+                            disabled={isPending}
+                        />
+                        <Button
+                            className="absolute top-0 right-0 cursor-pointer"
+                            type="button"
+                            variant="ghost"
+                            size="icon"
+                            onClick={() => setShowPassword(!showPassword)}
+                        >
+                            {showPassword ? <Eye /> : <EyeOff />}
+                        </Button>
+                    </div>
                 </div>
 
                 {!isLoginForm && (
                     <div className="flex flex-col space-y-1.5">
                         <Label htmlFor="confirmPassword">Confirm Password</Label>
-                        <Input
-                            id="confirmPassword"
-                            name="confirmPassword"
-                            placeholder="Enter your password again"
-                            type="password"
-                            required
-                            disabled={isPending}
-                        />
+                        <div className="relative">
+                            <Input
+                                id="confirmPassword"
+                                name="confirmPassword"
+                                placeholder="Enter your password again"
+                                type={showConfirmPs ? "text" : "password"}
+                                required
+                                disabled={isPending}
+                            />
+                            <Button
+                                className="absolute top-0 right-0 cursor-pointer"
+                                type="button"
+                                variant="ghost"
+                                size="icon"
+                                onClick={() => setShowConfirmPs(!showConfirmPs)}
+                            >
+                                {showConfirmPs ? <Eye /> : <EyeOff />}
+                            </Button>
+                        </div>
                     </div>
                 )}
             </CardContent>
@@ -136,7 +118,7 @@ const AuthForm: React.FC<Props> = ({ type }) => {
             <CardFooter className="flex flex-col mt-5 gap-6">
                 {/** For showing error on sign up page only for client side error */}
                 {!isLoginForm && clientValidationError && (
-                    <p className="text-red-500 text-sm mb-2">{ clientValidationError }</p>
+                    <p className="text-red-500 text-sm">{ clientValidationError }</p>
                 )}
 
                 <Button className="w-full cursor-pointer">
---
+++ src/utils/handleFormSubmit.ts
@@ -0,0 +1,61 @@
+import { TransitionStartFunction } from "react";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+import { loginAction, signUpAction } from "@/actions/users";
+import { toast } from "sonner";
+import { checkValidUserData } from "./validateData";
+
+const handleSubmit = (
+    formData: FormData,
+    isLoginForm: boolean,
+    router: AppRouterInstance,
+    setClientValidationError: React.Dispatch<React.SetStateAction<string | null>>,
+    startTransition: TransitionStartFunction
+): void => {
+    const email = formData?.get("email") as string;
+    const password = formData?.get("password") as string;
+    const confirmPassword = formData?.get("confirmPassword") as string;
+    const name = formData.get("name") as string;
+
+    // client side form validation on sign-up
+    if(!isLoginForm) {
+        const validationError = checkValidUserData(email, password, name, confirmPassword);
+
+        if(validationError) {
+            setClientValidationError(validationError);
+            return;
+        }
+    }
+    // clear the previous client error for login page
+    setClientValidationError(null);
+
+    startTransition(async () => {
+        let errorMessage;
+        let title;
+        let description;
+
+        if(isLoginForm) {
+            errorMessage = (await loginAction(email, password)).errorMessage;
+            title = "Logged In";
+            description = "You have been successfully logged in";
+        }else {
+            errorMessage = (await signUpAction(email, password, name)).errorMessage;
+            title = "Signed Up";
+            description = "Check your email for confirmation link"; 
+        }
+
+        if(!errorMessage) {
+            toast.success(title, {
+                description: description,
+            });
+
+            router.replace("/");
+        }else {
+            toast.error("Error", {
+                description: errorMessage,
+            });
+        }
+    });
+};
+
+export default handleSubmit;
